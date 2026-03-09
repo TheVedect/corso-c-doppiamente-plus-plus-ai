64647f5deb656460f66a306d3bd74360977df5ed
Gabriele Marchioni <ponziponzi1@gmail.com>
2026-03-09 14:17:40 +0100

    prova

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp3{
+    internal class Program{
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci un numero: ");
+            string numero = Console.ReadLine();
+            
+            Console.WriteLine("Inserisci un altro numero: ");
+            string altroNumero = Console.ReadLine();
+            int num1 = 0;
+            int num2 = 0;
+            try {
+                num1 = int.Parse(numero);
+                num2 = int.Parse(altroNumero);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
