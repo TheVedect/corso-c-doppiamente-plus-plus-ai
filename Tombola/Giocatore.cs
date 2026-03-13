86c1e29d4bf8e6c6368025cbe97dae57c057819f
Gabriele Marchioni <ponziponzi1@gmail.com>
2026-03-13 11:27:44 +0100

    mezza mattina

---
+++ Tombola/Giocatore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tombola{
+    public class Giocatore{
+
+        public string nome_giocatore;
+        public int numero_cartelle;
+        public List<Cartella> cartelle  = new List<Cartella>();
+
+        public Giocatore(string nome, int N) {
+            nome_giocatore = nome;
+            for (int i = 0; i < N; i++) {
+                Cartella cartella = new Cartella();
+                cartelle.Add(cartella);
+            }
+        }
+    }
+}
--- Tombola/Main.cs
+++ Tombola/Main.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 namespace Tombola{
     internal class Program{
 
-        public static void ControllaVincita(Cartella[] cartelle, Tabellone t) {
+        public static void ControllaVincita(Giocatore[] giocatori, Tabellone t) {
+            bool premio_vinto = false;
+            /*
             int contatore_tabellone = 0;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 9; j++) {
@@ -18,25 +20,44 @@ namespace Tombola{
                     return;
                 }
             }
-            foreach (Cartella cartella in cartelle) {
-                int contatore = 0;
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 9; j++) {
-                        if (cartella.cartella[i, j] == -1) {
-                            contatore++;
+            */
+            foreach (Giocatore giocatore in giocatori) {
+                    int indice_tabella = 1;
+                foreach (Cartella cartella in giocatore.cartelle) {
+                    int contatore = 0;
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 9; j++) {
+                            if (cartella.cartella[i, j] == -1) {
+                                contatore++;
+                                if (contatore == t.Prossima_vincita) {
+                                    Console.WriteLine("Il giocatore " + giocatore.nome_giocatore + " ha fatto " + contatore +
+                                                      " nella tabella numero " + indice_tabella + ".");
+                                    premio_vinto = true;
+                                    break;
+                                }
+                            }
+
+                            contatore = 0;
                         }
+                        if (premio_vinto) {
+                            break;
+                        }
+                        indice_tabella++;
                     }
 
-                    if (contatore == t.Prossima_vincita) {
-                        t.AggiornaVincita();
+                    if (premio_vinto) {
+                        break;
                     }
+                    
                 }
-                
+            }
+
+            if (premio_vinto) {
+                t.AggiornaVincita();
             }
         }
         private static void Main(string[] args) {
             Random rnd = new Random();
-            Console.WriteLine(rnd.Next());
             Tabellone tabellone = new Tabellone();
             //tabellone.VisualizzaTabellone();
             /*
@@ -44,13 +65,49 @@ namespace Tombola{
                 tabellone.PescaNumero();
             }
             tabellone.VisualizzaUsciti();
-            */
-            Cartella[] cartelle = new Cartella[2];
+            Cartella[] cartelle = new Cartella[3];
             for (int i = 0; i < cartelle.Length; i++) {
                 cartelle[i] = new Cartella();
                 cartelle[i].VisualizzaCartella();
                 Console.WriteLine();
             }
+            */
+            
+            Console.WriteLine("Quanti giocatori? ");
+            int numero_giocatori = int.Parse(Console.ReadLine());
+            Giocatore[] giocatori = new Giocatore[numero_giocatori];
+
+            for (int i = 0; i < numero_giocatori; i++) {
+                Console.WriteLine("Come si chiama il Giocatore numero " + (i + 1) + "?");
+                string nome_giocatore = Console.ReadLine();
+                Console.WriteLine("Quante cartelle ha " + nome_giocatore + "? ");
+                int numero_cartelle = int.Parse(Console.ReadLine());
+                giocatori[i] = new Giocatore(nome_giocatore, numero_cartelle);
+                foreach (Cartella cartella in giocatori[i].cartelle) {
+                    cartella.VisualizzaCartella();
+                    Console.WriteLine();
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                int numero_uscito = tabellone.PescaNumero();
+                foreach (Giocatore giocatore in giocatori) {
+                    foreach (Cartella cartella in giocatore.cartelle) {
+                        cartella.AggiornaCartella(numero_uscito);
+                    }
+                }
+                ControllaVincita(giocatori, tabellone);
+            }
+            
+            foreach (Giocatore giocatore in giocatori) {
+                Console.WriteLine("Cartelle di " +  giocatore.nome_giocatore + ":");
+                foreach (Cartella cartella in giocatore.cartelle) {
+                    cartella.VisualizzaCartella();
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            
         }
     }
 };
